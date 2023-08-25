19b56b1643440cfefe5accc43c960283210f364b
Suraj <surajgariya00@gmail.com>
2023-08-25 12:16:39 +0530

    First Commit

---
+++ lib/models/expense.dart
@@ -0,0 +1,7 @@
+class Expense {
+  Expense({required this.amount, required this.date, required this.title});
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+}
